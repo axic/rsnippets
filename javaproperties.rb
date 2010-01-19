86fd200569bd729f7f5e239794205a9297242900
Alex Beregszaszi <alex@rtfs.hu>
2010-01-19 01:09:01 +0000

    Tidy up and error checking

--- javaproperties.rb
+++ javaproperties.rb
@@ -1,33 +1,34 @@
 # Downloaded from http://devender.wordpress.com/2006/05/01/reading-and-writing-java-property-files-with-ruby/
 
-class JavaProps
-attr :file, :properties
+class JavaProperties
+  attr_accessor :file, :properties
 
-#Takes a file and loads the properties in that file
-def initialize file
-@file = file
-@properties = {}
-IO.foreach(file) do |line|
-@properties[$1.strip] = $2 if line =~ /([^=]*)=(.*)\/\/(.*)/ || line =~ /([^=]*)=(.*)/
-end
-end
+  def initialize(file)
+    @file = file
+    @properties = {}
 
-#Helpfull to string
-def to_s
-output = “File Name #{@file} \n”
-@properties.each {|key,value| output += ” #{key}= #{value} \n” }
-output
-end
+    begin
+      IO.foreach(file) do |line|
+        @properties[$1.strip] = $2 if line = ~ /([^=]*)=(.*)\/\/(.*)/ || line =~ /([^=]*)=(.*)/
+      end
+    rescue
+    end
+  end
 
-#Write a property
-def write_property (key,value)
-@properties[key] = value
-end
+  def to_s
+    output = "File name #{@file}\n"
+    @properties.each { |key, value| output += " #{key} = #{value}\n" }
+    output
+  end
 
-#Save the properties back to file
-def save
-file = File.new(@file,”w+”)
-@properties.each {|key,value| file.puts “#{key}=#{value}\n” }
-end
+  def add(key, value)
+    return unless key.length > 0 and value.length > 0
+    @properties[key] = value
+  end
 
+  def save
+    file = File.new(@file, "w+")
+    @properties.each { |key, value| file.puts "#{key}=#{value}\n" }
+    file.close
+  end
 end
